6ffe6fdf47efee91e27364932b2594c61bedb3b0
vishal <vishaldeepverma@live.in>
2022-07-05 23:20:49 +0530

    fix

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { userAuth } from '../utils/userAuth';
-const server = process.env.server || "http://localhost:4000/api/v1";
+const server = process.env.server || "https://sub.deeponlineseller.com/api/v1";
 
 let token = userAuth();
 export const serverV1Instance = axios.create({
